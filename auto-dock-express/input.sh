af2fdf92352309d926ac5a377625ba30aadff529
Jeff Lu <ljeff@us.ibm.com>
2022-11-02 22:12:16 -0400

    update Dockerfiles

--- auto-dock-express/input.sh
+++ auto-dock-express/input.sh
@@ -40,7 +40,7 @@ else
 fi
 
 export HORIZON_URL="http://localhost:8081"
-watch hzn agreement list    
+# watch hzn agreement list    
 # oh deploy setup --org $org_id
 #docker run -v /var/run/docker.sock:/var/run/docker.sock -ti docker
 # echo "Docker login"
